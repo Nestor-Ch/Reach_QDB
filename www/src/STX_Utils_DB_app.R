bfdf65879d4288c697e22e6a6ab3a2fef6e23250
Nestor-Ch <132923140+Nestor-Ch@users.noreply.github.com>
2023-09-29 12:10:29 +0300

    Update STX_Utils_DB_app.R

--- www/src/STX_Utils_DB_app.R
+++ www/src/STX_Utils_DB_app.R
@@ -2,7 +2,34 @@
 options(shiny.maxRequestSize=30*1024^2)
 
 # Define functions--------------------------------------------
-
+# function that will be used for the round numbers in the research cycle table to make them pretty
+numbers_to_string <- function(vec) {
+  
+  if (length(vec)==1){
+    return(as.character(vec))
+  }else{
+    
+    ranges <- c()
+    start_range <- vec[1]
+    for (i in 2:length(vec)) {
+      if (vec[i] != vec[i - 1] + 1) {
+        if (start_range != vec[i - 1]) {
+          ranges <- append(ranges, paste(start_range, vec[i - 1], sep = "-"))
+        } else {
+          ranges <- append(ranges, as.character(start_range))
+        }
+        start_range <- vec[i]
+      }
+    }
+    # Handle the last range
+    if (start_range != vec[length(vec)]) {
+      ranges <- append(ranges, paste(start_range, vec[length(vec)], sep = "-"))
+    } else {
+      ranges <- append(ranges, as.character(start_range))
+    }
+    result <- paste(ranges, collapse = ",")
+    return(result)}
+}
 # define a function that cleans columns
 column_cleaner <- function(df, name, label) {
   new_input = df %>%
